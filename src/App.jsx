fd9a2e2c49d4bee6fdf4f3bb71eb451bb5e3b43d
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-28 11:12:05 +0530

    login page frontend completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
 import React from 'react'
+import Login from './components/Auth/Login'
 
 const App = () => {
   return (
     <div>
-       <h1 className="text-3xl font-bold underline">
-         Hello world!
-       </h1>
+       <Login/>
     </div>
   )
 }
