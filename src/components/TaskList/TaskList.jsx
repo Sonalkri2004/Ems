b961bd042dd1da33ff547acc208a7e426e367aa3
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-12-04 18:48:14 +0530

    TaskList added.

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,68 +1,29 @@
-import React from "react";
+import React from 'react'
+import AcceptTask from './AcceptTask'
+import NewTask from './NewTask'
+import CompleteTask from './CompleteTask'
+import FailedTask from './FailedTask'
 
-const TaskList = () => {
-  return (
-    <div
-      id="tasklist"
-      className="h-[50%] overflow-x-auto flex items-center justify-start gap-5  flex-nowrap w-full py-1 mt-16"
-    >
-      <div className=" flex-shrink-0 h-full w-[300px] rounded-xl bg-red-400">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-red-600 text-sm px-5 py-1 my-4 mx-2 rounded">High</h3>
-          <h4 className="text-sm pr-3">20 feb 2024</h4>
-        </div>
-        <h2 className="mt-5 text-2xl px-2 font-semibold">Make a youtube video</h2>
-        <p className="text-sm mt-2 px-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
-          cupiditate?Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, vero.
-        </p>
-      </div>
-      <div className=" flex-shrink-0 h-full w-[300px] rounded-xl bg-blue-400">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-blue-600 text-sm px-5 py-1 my-4 mx-2 rounded">High</h3>
-          <h4 className="text-sm pr-3">20 feb 2024</h4>
-        </div>
-        <h2 className="mt-5 text-2xl px-2 font-semibold">Make a youtube video</h2>
-        <p className="text-sm mt-2 px-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
-          cupiditate?Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, vero.
-        </p>
-      </div>
-      <div className=" flex-shrink-0 h-full w-[300px] rounded-xl bg-green-400">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-green-600 text-sm px-5 py-1 my-4 mx-2 rounded">High</h3>
-          <h4 className="text-sm pr-3">20 feb 2024</h4>
-        </div>
-        <h2 className="mt-5 text-2xl px-2 font-semibold">Make a youtube video</h2>
-        <p className="text-sm mt-2 px-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
-          cupiditate?Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, vero.
-        </p>
-      </div>
-      <div className=" flex-shrink-0 h-full w-[300px] rounded-xl bg-yellow-400">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-yellow-600 text-sm px-5 py-1 my-4 mx-2 rounded">High</h3>
-          <h4 className="text-sm pr-3">20 feb 2024</h4>
-        </div>
-        <h2 className="mt-5 text-2xl px-2 font-semibold">Make a youtube video</h2>
-        <p className="text-sm mt-2 px-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
-          cupiditate?Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, vero.
-        </p>
-      </div>
-      <div className=" flex-shrink-0 h-full w-[300px] rounded-xl bg-orange-400">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-orange-600 text-sm px-5 py-1 my-4 mx-2 rounded">High</h3>
-          <h4 className="text-sm pr-3">20 feb 2024</h4>
-        </div>
-        <h2 className="mt-5 text-2xl px-2 font-semibold">Make a youtube video</h2>
-        <p className="text-sm mt-2 px-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
-          cupiditate?Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, vero.
-        </p>
-      </div>
-   </div>   
-  );
-};
+const TaskList = ({ data }) => {
+    return (
+        <div id='tasklist' className='h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-1 mt-16'>
+            {data.tasks.map((elem, idx) => {
+                if (elem.active) {
+                    return <AcceptTask key={idx} data={elem} />
+                }
+                if (elem.newTask) {
+                    return <NewTask key={idx} data={elem} />
+                }
+                if (elem.completed) {
+                    return <CompleteTask key={idx} data={elem} />
+                }
+                if (elem.failed) {
+                    return <FailedTask key={idx} data={elem} />
+                }
 
-export default TaskList;
+            })}
+        </div>
+    )
+}
+
+export default TaskList
